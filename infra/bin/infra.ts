f6d6f69f4b8952befa7542eff99366fb2fcf7a3d
Ruben <ruben_cancino@epam.com>
2025-10-01 22:48:28 -0600

    feat: complete task 3.1 and 3.2

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { DeployWebAppStack } from '../lib/deploy-web-app-stack';
+import { ProductServiceStack } from '../lib/product-service-stack';
 
 const app = new cdk.App();
 new DeployWebAppStack(app, 'DeployWebAppStack', {});
+new ProductServiceStack(app, 'ProductServiceStack', {});
+
+// Some final Tags
+cdk.Tags.of(app).add('Course', 'CloudeX: AWS Practitioner JS');
+cdk.Tags.of(app).add('Environment', 'prod');
+cdk.Tags.of(app).add('Owner', 'Ruben Cancino');
+
