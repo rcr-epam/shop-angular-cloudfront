1e2ae1b8f75a2651fa7f3440a43a31ef2ab3877d
Ruben <ruben_cancino@epam.com>
2025-10-07 21:13:37 -0600

    feat: complete Task 4.3

--- infra/lib/product-service-stack.ts
+++ infra/lib/product-service-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 import * as path from 'path';
@@ -9,50 +9,94 @@ import { Construct } from 'constructs';
 
 export type ProductServiceStackProps = cdk.StackProps & {
   productsTable: string;
-  stockTable: string
+  stockTable: string;
 };
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: ProductServiceStackProps) {
     super(scope, id, props);
 
     // The Products table was created using AWS Console so we import it here
-    const productsTable = dynamodb.Table.fromTableName(this, 'ExistingTable', props.productsTable);
-    const stockTable = dynamodb.Table.fromTableName(this, 'ExistingStockTable', props.stockTable);
-    
-    const getProductsLambda = new lambdaNodejs.NodejsFunction(this, 'GetProducts', {
-      entry: path.join(__dirname, '../src/lambda','get-products-list.ts'),
-      runtime: lambda.Runtime.NODEJS_22_X,
-      timeout: cdk.Duration.seconds(30),
-      memorySize: 256,
-      bundling: {
-        minify: true,
-        sourceMap: true
+    const productsTable = dynamodb.Table.fromTableName(
+      this,
+      'ExistingTable',
+      props.productsTable,
+    );
+    const stockTable = dynamodb.Table.fromTableName(
+      this,
+      'ExistingStockTable',
+      props.stockTable,
+    );
+
+    const getProductsLambda = new lambdaNodejs.NodejsFunction(
+      this,
+      'GetProducts',
+      {
+        entry: path.join(__dirname, '../src/lambda', 'get-products-list.ts'),
+        runtime: lambda.Runtime.NODEJS_22_X,
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 256,
+        bundling: {
+          minify: true,
+          sourceMap: true,
+        },
+        environment: {
+          PRODUCTS_TABLE_NAME: props.productsTable,
+          STOCKS_TABLE_NAME: props.stockTable,
+        },
       },
-      environment: {
-        PRODUCTS_TABLE_NAME: props.productsTable,
-        STOCKS_TABLE_NAME: props.stockTable
-      },
-    });
+    );
     getProductsLambda.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
     // grant read and write permissions to the Lambda function
     productsTable.grantReadWriteData(getProductsLambda);
     stockTable.grantReadWriteData(getProductsLambda);
 
     // Lambda function for getting product by ID
-    const getProductByIdLambda = new lambdaNodejs.NodejsFunction(this, 'GetProductById', {
-      entry: path.join(__dirname, '../src/lambda','get-product-by-id.ts'),
-      runtime: lambda.Runtime.NODEJS_22_X,
-      timeout: cdk.Duration.seconds(30),
-      memorySize: 256,
-      bundling: {
-        minify: true,
-        sourceMap: true
-      }
-    });
+    const getProductByIdLambda = new lambdaNodejs.NodejsFunction(
+      this,
+      'GetProductById',
+      {
+        entry: path.join(__dirname, '../src/lambda', 'get-product-by-id.ts'),
+        runtime: lambda.Runtime.NODEJS_22_X,
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 256,
+        bundling: {
+          minify: true,
+          sourceMap: true,
+        },
+        environment: {
+          PRODUCTS_TABLE_NAME: props.productsTable,
+          STOCKS_TABLE_NAME: props.stockTable,
+        },
+      },
+    );
     getProductByIdLambda.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
     // grant read and write permissions to the Lambda function
     productsTable.grantReadWriteData(getProductByIdLambda);
     stockTable.grantReadWriteData(getProductByIdLambda);
+
+    // Lambda function for creating a new product
+    const createProductLambda = new lambdaNodejs.NodejsFunction(
+      this,
+      'CreateProduct',
+      {
+        entry: path.join(__dirname, '../src/lambda', 'create-product.ts'),
+        runtime: lambda.Runtime.NODEJS_22_X,
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 256,
+        bundling: {
+          minify: true,
+          sourceMap: true,
+        },
+        environment: {
+          PRODUCTS_TABLE_NAME: props.productsTable,
+          STOCKS_TABLE_NAME: props.stockTable,
+        },
+      },
+    );
+    createProductLambda.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    // grant read and write permissions to the Lambda function
+    productsTable.grantReadWriteData(createProductLambda);
+    stockTable.grantReadWriteData(createProductLambda);
 
     // Create API Gateway REST API with Swagger documentation
     const api = new apigateway.RestApi(this, 'ProductsAPI', {
@@ -64,7 +108,7 @@ export class ProductServiceStack extends cdk.Stack {
       defaultCorsPreflightOptions: {
         allowOrigins: ['https://d1cifp48n770r7.cloudfront.net/'],
         allowMethods: apigateway.Cors.ALL_METHODS,
-      }
+      },
     });
 
     // Define the API model for Product
@@ -78,10 +122,10 @@ export class ProductServiceStack extends cdk.Stack {
           title: { type: apigateway.JsonSchemaType.STRING },
           description: { type: apigateway.JsonSchemaType.STRING },
           price: { type: apigateway.JsonSchemaType.NUMBER },
-          count: { type: apigateway.JsonSchemaType.INTEGER }
+          count: { type: apigateway.JsonSchemaType.INTEGER },
         },
-        required: ['id', 'title', 'price', 'count']
-      }
+        required: ['id', 'title', 'price', 'count'],
+      },
     });
 
     // Define response models
@@ -93,10 +137,10 @@ export class ProductServiceStack extends cdk.Stack {
         properties: {
           success: { type: apigateway.JsonSchemaType.BOOLEAN },
           error: { type: apigateway.JsonSchemaType.STRING },
-          message: { type: apigateway.JsonSchemaType.STRING }
+          message: { type: apigateway.JsonSchemaType.STRING },
         },
-        required: ['success', 'error', 'message']
-      }
+        required: ['success', 'error', 'message'],
+      },
     });
 
     const productsResponseModel = api.addModel('ProductsResponse', {
@@ -108,12 +152,12 @@ export class ProductServiceStack extends cdk.Stack {
           success: { type: apigateway.JsonSchemaType.BOOLEAN },
           data: {
             type: apigateway.JsonSchemaType.ARRAY,
-            items: { type: apigateway.JsonSchemaType.OBJECT }
+            items: { type: apigateway.JsonSchemaType.OBJECT },
           },
-          total: { type: apigateway.JsonSchemaType.INTEGER }
+          total: { type: apigateway.JsonSchemaType.INTEGER },
         },
-        required: ['success', 'data', 'total']
-      }
+        required: ['success', 'data', 'total'],
+      },
     });
 
     const productResponseModel = api.addModel('ProductResponse', {
@@ -123,94 +167,113 @@ export class ProductServiceStack extends cdk.Stack {
         type: apigateway.JsonSchemaType.OBJECT,
         properties: {
           success: { type: apigateway.JsonSchemaType.BOOLEAN },
-          data: { type: apigateway.JsonSchemaType.OBJECT }
+          data: { type: apigateway.JsonSchemaType.OBJECT },
         },
-        required: ['success', 'data']
-      }
+        required: ['success', 'data'],
+      },
     });
 
     // Create /products resource
     const products = api.root.addResource('products');
 
     // GET /products
-    products.addMethod('GET', new apigateway.LambdaIntegration(getProductsLambda), {
-      methodResponses: [
-        {
-          statusCode: '200',
-          responseModels: {
-            'application/json': productsResponseModel
-          }
-        },
-        {
-          statusCode: '500',
-          responseModels: {
-            'application/json': errorResponseModel
-          }
-        }
-      ]
-    });
+    products.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getProductsLambda),
+      {
+        methodResponses: [
+          {
+            statusCode: '200',
+            responseModels: {
+              'application/json': productsResponseModel,
+            },
+          },
+          {
+            statusCode: '500',
+            responseModels: {
+              'application/json': errorResponseModel,
+            },
+          },
+        ],
+      },
+    );
 
     // Create /products/{id} resource
     const productById = products.addResource('{id}');
 
     // GET /products/{id}
-    productById.addMethod('GET', new apigateway.LambdaIntegration(getProductByIdLambda), {
-      requestParameters: {
-        'method.request.path.id': true
+    productById.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getProductByIdLambda),
+      {
+        requestParameters: {
+          'method.request.path.id': true,
+        },
+        methodResponses: [
+          {
+            statusCode: '200',
+            responseModels: {
+              'application/json': productResponseModel,
+            },
+          },
+          {
+            statusCode: '400',
+            responseModels: {
+              'application/json': errorResponseModel,
+            },
+          },
+          {
+            statusCode: '404',
+            responseModels: {
+              'application/json': errorResponseModel,
+            },
+          },
+          {
+            statusCode: '500',
+            responseModels: {
+              'application/json': errorResponseModel,
+            },
+          },
+        ],
       },
-      methodResponses: [
-        {
-          statusCode: '200',
-          responseModels: {
-            'application/json': productResponseModel
-          }
-        },
-        {
-          statusCode: '400',
-          responseModels: {
-            'application/json': errorResponseModel
-          }
-        },
-        {
-          statusCode: '404',
-          responseModels: {
-            'application/json': errorResponseModel
-          }
-        },
-        {
-          statusCode: '500',
-          responseModels: {
-            'application/json': errorResponseModel
-          }
-        }
-      ]
-    });
+    );
+
+    // POST /products
+    products.addMethod(
+      'POST',
+      new apigateway.LambdaIntegration(createProductLambda),
+    );
 
     // Output the API URL
     new cdk.CfnOutput(this, 'ApiUrl', {
       value: api.url,
-      description: 'URL of the Products API'
+      description: 'URL of the Products API',
     });
 
-    new cdk.CfnOutput(this, "ProductsApiUrl", {
+    new cdk.CfnOutput(this, 'ProductsApiUrl', {
       value: `${api.url}products`,
-      description: "The URL of the /products resource",
+      description: 'The URL of the /products resource',
     });
 
-    new cdk.CfnOutput(this, "ProductsApiByIdUrl", {
+    new cdk.CfnOutput(this, 'ProductsApiByIdUrl', {
       value: `${api.url}products/{id}`,
-      description: "The URL of the /products/{id} resource",
+      description: 'The URL of the /products/{id} resource',
+    });
+
+    new cdk.CfnOutput(this, 'CreateProductApiUrl', {
+      value: `${api.url}products`,
+      description: 'The URL to create a new product',
     });
 
     // Output Swagger/OpenAPI documentation URL
     new cdk.CfnOutput(this, 'SwaggerUrl', {
       value: `${api.url}swagger`,
-      description: 'Swagger documentation URL'
+      description: 'Swagger documentation URL',
     });
 
     // Some final Tags
-    cdk.Tags.of(this).add("Module 3", "Serverless");
-    cdk.Tags.of(this).add("Task 3", "Products API");
+    cdk.Tags.of(this).add('Module 3', 'Serverless');
+    cdk.Tags.of(this).add('Task 3', 'Products API');
 
     // Cleanup all log groups
     // cdk.Aspects.of(this).add({
--- infra/src/lambda/data-access.ts
+++ infra/src/lambda/data-access.ts
@@ -1,17 +1,23 @@
-/** ES6 Data access layer for interacting with DynamoDB products and stock tables 
+/** ES6 Data access layer for interacting with DynamoDB products and stock tables
  * Abstracted to its own module for easier testing and reuse.
- * 
+ *
  * @module data-access
- * 
- * */ 
+ *
+ * */
 import {
   DynamoDBClient,
   ScanCommand,
   ScanCommandInput,
 } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  TransactWriteCommand,
+  TransactWriteCommandInput,
+} from '@aws-sdk/lib-dynamodb';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { Product, Stock } from '../domain/product';
+import * as uuid from 'uuid';
 
 // DynamoDB client and Document client initialization Only available for functions in this module
 const dynamoDb = new DynamoDBClient();
@@ -22,7 +28,7 @@ const stockTable = process.env.STOCKS_TABLE_NAME || 'stock';
 
 /**
  * Fetches a product by its ID from the products table.
- * 
+ *
  * @param id The ID of the product to fetch
  * @returns The product if found, otherwise an error
  */
@@ -49,7 +55,7 @@ export async function fetchProductById(
 
 /**
  * Fetches a list of products from the products table and merges in their stock counts from the stocks table.
- * 
+ *
  * @returns A list of products with their stock counts merged in
  */
 export async function fetchProducts(): Promise<{
@@ -86,13 +92,13 @@ export async function fetchProducts(): Promise<{
 }
 
 /**
- * Fetches stock data from the stocks table and returns a map of product_id to count. 
+ * Fetches stock data from the stocks table and returns a map of product_id to count.
  * This allows efficient lookup of stock counts when merging with product data.
- * 
+ *
  * @returns returns a map of product_id to count
  */
 export async function fetchStocks(): Promise<{
-  stocks: Record<string,number>;
+  stocks: Record<string, number>;
   error?: Error;
 }> {
   const params: ScanCommandInput = {
@@ -103,15 +109,74 @@ export async function fetchStocks(): Promise<{
   try {
     const command = new ScanCommand(params);
     const result = await documentClient.send(command);
-    const stocks = (result.Items ?? []).map(
-      (item) => unmarshall(item) as Stock,
-    ).reduce(
-      (acc, stock) => ((acc[stock.product_id] = stock.count), acc),
-      {} as Record<string, number>,
-    );
+    const stocks = (result.Items ?? [])
+      .map((item) => unmarshall(item) as Stock)
+      .reduce(
+        (acc, stock) => ((acc[stock.product_id] = stock.count), acc),
+        {} as Record<string, number>,
+      );
     return { stocks };
   } catch (error) {
     console.error('Error fetching stocks:', error);
     return { stocks: {}, error: new Error('Could not fetch stocks') };
+  }
+}
+
+/**
+ *
+ * Creates a new product in the products table and its corresponding stock entry in the stocks table using a transactional write.
+ * Ensures that both the product and stock entries are created atomically.
+ * If either write fails, neither entry is created.
+ *
+ * @param productData - product data to create
+ * @returns  the created product or an error if creation failed
+ *
+ */
+export async function createProduct(
+  productData: Product,
+): Promise<{ product?: Product; error?: Error }> {
+  const productId = uuid.v4();
+
+  const productItem = {
+    id: productId,
+    title: productData.title,
+    description: productData.description,
+    price: productData.price,
+  };
+
+  const stockItem = {
+    product_id: productId,
+    count: productData.count ?? 0,
+  };
+
+  const transactParams: TransactWriteCommandInput = {
+    TransactItems: [
+      {
+        Put: {
+          TableName: productsTable,
+          Item: productItem,
+          ConditionExpression: 'attribute_not_exists(id)', // Ensure the product doesn't already exist
+        },
+      },
+      {
+        Put: {
+          TableName: stockTable,
+          Item: {
+            productId: productId,
+            quantity: productData.count ?? 0,
+          },
+          ConditionExpression: 'attribute_not_exists(productId)', // Ensure the stock entry doesn't already exist
+        },
+      },
+    ],
+  };
+
+  try {
+    const command = new TransactWriteCommand(transactParams);
+    await documentClient.send(command);
+    return { product: { ...productItem, count: stockItem.count } as Product };
+  } catch (error) {
+    console.error('Error in transactional write (putProductWithStock):', error);
+    return { error: new Error('Could not create product') };
   }
 }
